2627c0b8b7802aa587aad6a5344bc43c43edde4e
Hyunmin Jung <jung@hyunmin.dev>
2021-01-11 15:06:03 +0900

    Add input output function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
 
+
+using namespace std;
+
+int solution(int island_count, vector<vector<int>> cost_vec) {
+  int result = 0;
+  return result;
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cout << solution(4, {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}}) << endl;
   return 0;
 }
